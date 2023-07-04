a1a73296f8d51d7b1979065a52c911d78cf7a589
Per Eriksson <per@interapp.se>
2023-07-05 00:33:23 +0200

    minor cleanup

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
 from flask import Flask, render_template, jsonify
-from datetime import *
-import functools
 import locale
 from templates.filters import format_amount
-#from util.api import get_deals, get_deals_per_month, deal_is_won_last_year, get_value_per_company, updated_company_statuses, get_avg_deal_value_for_last_year
 from util.api import Api
 
 locale.setlocale(locale.LC_ALL, 'sv_se')
-
 
 app = Flask(__name__, static_url_path='/static')
 
@@ -30,16 +26,9 @@ def deals_per_month():
 def home():
     api = Api()
 
-    # Avg all deals won last year
     avg_deal_value = api.get_avg_deal_value_for_last_year()
-
-    # Number of won all deals per month last year
     deals_per_month = api.get_deals_per_month()
-
-    # Total value of won all deals per customer last year
     value_per_company = api.get_value_per_company()
-
-    # Updated company statuses
     updated_companies = api.updated_company_statuses()
 
     return render_template(
